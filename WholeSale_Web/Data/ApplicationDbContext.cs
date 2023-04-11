1d6d89e30b12403b7555d877584c37c09eb5c216
Harsh <jadhavharsh.217@gmail.com>
2023-04-11 07:52:50 +0530

    Add project files.

---
+++ WholeSale_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WholeSale_Web.Models;
+
+namespace WholeSale_Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Cutlery", DisplayOrder=1 },
+                new Category { Id=2, Name="Clothing", DisplayOrder=2 },
+                new Category { Id=3, Name="Stationary", DisplayOrder=3 }
+                );
+        }
+    }
+}
