fe9fd0d692eeb1e1180a20a3ebb5f6dc779bdfbb
Harsh <jadhavharsh.217@gmail.com>
2023-04-11 22:00:26 +0530

    Create Category Added Successfully

--- WholeSale_Web/Controllers/CategoryController.cs
+++ WholeSale_Web/Controllers/CategoryController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WholeSale_Web.Data;
+using WholeSale_Web.Models;
 
 namespace WholeSale_Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
