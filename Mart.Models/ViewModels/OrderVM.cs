4abae1057ed8b4e65dbb391026f400efc204bfa6
Harsh <jadhavharsh.217@gmail.com>
2023-05-01 21:04:20 +0530

    Order Management and Filtering done

---
+++ Mart.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,8 @@
+﻿namespace Mart.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
